51b980b38afe29f9e66d81a700e5ec62b9197dc9
Nouman Syed <syednouman2114@gmail.com>
2023-11-10 21:50:47 -0500

    another bug :/

--- utils/fileSaver.js
+++ utils/fileSaver.js
@@ -7,6 +7,6 @@ import fs from 'fs-extra';
  **/
 
 export const saveToOutputFile = async (htmlContent) => {
-  fs.writeFileSync('output.html', htmlContent, 'utf-8');
+  fs.outputFileSync('output.html', htmlContent, 'utf-8');
   console.log('HTML content saved to output.html');
 }
