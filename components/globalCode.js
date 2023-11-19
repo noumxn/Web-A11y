8c3acb8f345f3b2051a4eff920210c858967d7d6
Ashna Razdan <ashnarazdan123@gmail.com>
2023-11-19 12:49:37 -0500

    Global Code B

--- components/globalCode.js
+++ components/globalCode.js
@@ -5,16 +5,32 @@ export const checkGlobalCode = (document) => {
     const allHtmlTags = document.querySelectorAll("html");
 
     for (const htmlTag of allHtmlTags) {
+      // Global Code A: Use a lang attribute on the html element.
       const langAttribute = htmlTag.getAttribute("lang");
-
       if (!langAttribute) {
         let output = chalk.red("Missing lang attribute on the <html> tag:");
         output += `\n${chalk.cyan(htmlTag.outerHTML)}`;
         return output;
       }
+
+      //Global Code B: Provide a unique title for each page or view.
+      const titleElements = document.querySelectorAll("title");
+      if (!titleElements || titleElements.length === 0) {
+        return chalk.red("Missing <title> tag in the HTML file!");
+      }
+
+      const titles = Array.from(titleElements).map((title) =>
+        title.textContent.trim()
+      );
+
+      const uniqueTitles = [...new Set(titles)];
+
+      if (titles.length !== uniqueTitles.length) {
+        return chalk.red(`Non-unique title found: ${titles.join(", ")}`);
+      }
     }
 
-    return chalk.green("Lang attribute exists for all <html> tags!");
+    return chalk.green("Lang attribute and unique title exist for all <html> tags!");
   } catch (e) {
     return `${chalk.red("Error parsing the HTML file:")}${e}`;
   }
--- tests/globalCode.test.js
+++ tests/globalCode.test.js
@@ -5,13 +5,13 @@ import { checkGlobalCode } from "../components/globalCode.js";
 import { expect } from "chai";
 
 describe("Lang Attribute Tests", () => {
-  it("should pass when lang attribute exists on <html> tag", () => {
+  it("should pass when lang attribute and unique title exist on <html> tag", () => {
     const html = `<!DOCTYPE html>
       <html lang="en">
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Lang Attribute</title>
+          <title>Unique Title</title>
         </head>
         <body>
           <p>Hello World</p>
@@ -23,7 +23,7 @@ describe("Lang Attribute Tests", () => {
 
     const result = checkGlobalCode(document);
     expect(result).to.equal(
-      chalk.green("Lang attribute exists for all <html> tags!"),
+      chalk.green("Lang attribute and unique title exist for all <html> tags!"),
     );
   });
 
@@ -33,7 +33,7 @@ describe("Lang Attribute Tests", () => {
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Lang Attribute</title>
+          <title>Unique Title</title>
         </head>
         <body>
           <p>Hello World</p>
@@ -46,6 +46,29 @@ describe("Lang Attribute Tests", () => {
     const result = checkGlobalCode(document);
     expect(result).to.include(
       chalk.red("Missing lang attribute on the <html> tag:"),
+    );
+  });
+
+  it("should fail when title is not unique", () => {
+    const html = `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Not Unique Title</title>
+          <title>Not Unique Title</title>
+        </head>
+        <body>
+          <p>Hello World</p>
+        </body>
+      </html>`;
+
+    const dom = new JSDOM(html);
+    const { document } = dom.window;
+
+    const result = checkGlobalCode(document);
+    expect(result).to.include(
+      chalk.red("Non-unique title found: Not Unique Title, Not Unique Title"),
     );
   });
 });
