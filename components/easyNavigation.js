630fdce0a32671b00e13a563531a916f245ead10
Ashna Razdan <ashnarazdan123@gmail.com>
2023-11-18 22:27:37 -0500

    linted easyNav

--- components/easyNavigation.js
+++ components/easyNavigation.js
@@ -1,4 +1,4 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
 /*
  * @description NAVIGABLE: Verify the availability of skip links and landmarks for easy navigation
@@ -8,19 +8,19 @@ export const checkSkipLinks = (document) => {
   try {
     const images = document.querySelectorAll('a, [role="a"]');
     let hasSkip = false;
-    images.forEach((element, index) => {
-        var linkPattern = new RegExp("#main[\\w-]*", "i");
-        const link = element.getAttribute('href');
-        if (linkPattern.test(link)) {
-            hasSkip = true;
-        }
+    images.forEach((element, _index) => {
+      var linkPattern = new RegExp("#main[\\w-]*", "i");
+      const link = element.getAttribute("href");
+      if (linkPattern.test(link)) {
+        hasSkip = true;
+      }
     });
     if (hasSkip) {
-      return chalk.green('Skip Link test passed!');
+      return chalk.green("Skip Link test passed!");
     } else {
-      return chalk.red('Missing skip link');
+      return chalk.red("Missing skip link");
     }
   } catch (e) {
-    return `${chalk.red('Error parsing the HTML file:')}${e}`;
+    return `${chalk.red("Error parsing the HTML file:")}${e}`;
   }
 };
