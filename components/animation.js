18389172c541980f74251700710717f60aec91b8
invissy <72313458+invissy@users.noreply.github.com>
2023-12-14 20:23:44 -0500

    animation flash

---
+++ components/animation.js
@@ -0,0 +1,138 @@
+import chalk from "chalk";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const cssPath = "../output.css";
+const scriptDirectory = __dirname;
+const cssFilePath = path.resolve(scriptDirectory, cssPath);
+
+/**
+ * @function checkFlashes
+ * @param {Document} document
+ * @return {string} Success message if 
+ * @return {string} Failure message if 
+ * @throws Error parsing the HTML file
+ * @desc ERROR
+ * @description
+ * 14.A
+ * Ensure animations are subtle and do not flash too much
+ * Certain kinds of strobing or flashing animations will trigger seizures
+ * Others may be distracting and disruptive, especially for certain kinds of
+ * cognitive disability such as ADHD
+ **/
+
+export const checkAnimationFlash = async (document) => {
+    try {
+        let output = "Make sure that all animations do not flash more than 3 times per second";
+        const cssContent = await fs.readFile(cssFilePath, "utf8");
+
+        const flashViolations = checkFlashes(cssContent);
+
+        if (flashViolations.length != 0) {
+            flashViolations.forEach(violation => {
+                output += "Animation named: '" + violation;
+            });
+        }
+
+        if (output.length == 0) {
+            return chalk.green("Animations do not exceed flash limit!")
+        }
+        else { return chalk.red(output); }
+
+    } catch (err) {
+        return `${err}${chalk.red("Error processing the document or CSS file:")}`;
+    }
+};
+
+
+function countKeyframes(cssCode, animationName) {
+    const keyframesPattern = new RegExp(`@keyframes\\s+${animationName}\\s*{([^}]*)}`, 'g');
+
+    const keyframesMatches = cssCode.match(keyframesPattern);
+
+    if (keyframesMatches) {
+        // Extract the keyframes block
+        const keyframesBlock = keyframesMatches[0];
+
+        // Count the number of frames
+        const framesCount = (keyframesBlock.match(/(?:\d+%)\s*{/g) || []).length;
+
+        return framesCount;
+    }
+
+    // If the animation name is not found
+    return 0;
+}
+
+
+function checkFlashes(cssContent) {
+    const kfPattern = /@keyframes\s+([\w-]+)\s*{[^}]*}/g;
+    const animPattern = /\s*animation\s*:\s*([^;]+);/g;
+    const durPattern = /\b(\d*\.?\d+)(s|ms)\b/g;
+    const flashesPerSec = 3; //limit flashes to 3 or less each second
+    const animations = [];
+
+    // Find keyframes
+    let keyframes;
+    while ((keyframes = kfPattern.exec(cssContent)) !== null) {
+        const animationName = keyframes[1];
+        const frames = countKeyframes(cssContent, animationName);
+        animations.push({ name: animationName, duration: 0, frames: frames });
+    }
+
+    // Find animations in regular rules
+    let matches;
+    while ((matches = animPattern.exec(cssContent)) !== null) {
+        const properties = matches[1].split(',');
+        for (const prop of properties) {
+            const parts = prop.trim().split(' ');
+            const animationName = parts[0];
+            const durationMatch = parts.find(part => durPattern.test(part));
+
+            if (durationMatch) {
+                const [duration, unit] = durationMatch.match(/\b(\d*\.?\d+)(s|ms)\b/);
+                const durationInSeconds = unit;
+
+                const animation = animations.find(a => a.name === animationName);
+                if (animation) {
+                    animation.duration = Math.max(animation.duration, durationInSeconds);
+                }
+            }
+        }
+    }
+    const violations = animations
+        .filter(animation => animation.frames / animation.duration > flashesPerSec)
+        .map(animation => animation.name);
+
+    animations.forEach(element => {
+        console.log(element.name);
+        console.log(element.frames);
+        console.log(element.duration);
+    });
+
+    return violations;
+}
+
+// Example usage
+const cssContent = `
+@keyframes fadeIn {
+    0% { opacity: 0; }
+    50% { opacity: 0.5; }
+    100% { opacity: 1; }
+  }
+  
+  @keyframes slideIn {
+    0% { transform: translateX(-100%); }
+    50% { transform: translateX(-50%); }
+    100% { transform: translateX(0); }
+  }
+  
+  .element {
+    animation: fadeIn 2s ease-in-out, slideIn 1s linear;
+  }
+  `;
+
+console.log(checkFlashes(cssContent));
